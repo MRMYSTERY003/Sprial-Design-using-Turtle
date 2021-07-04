9d87d219edd0cf10fa03d148f7a0380eb8a0b521
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2021-07-04 09:39:31 +0530

    adding the comments

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import turtle
+'''project by Mr_Myster
+Check out our yt channel 
+link in readme file'''
 
-colors = ['red', 'yellow', 'green', 'purple', 'orange', 'blue']
+import turtle   #importing turtle mofdule
+
+colors = ['red', 'yellow', 'green', 'purple', 'orange', 'blue']     #setting the colors
+
+
 t = turtle.Pen()
-t.shape('turtle')
-turtle.bgcolor('black')
+t.shape('turtle')       #setting the shape of the pen as turtle
+turtle.bgcolor('black')     #setting the bg color as black
 for x in range(200):
-    t.pencolor(colors[x%6])
+    t.pencolor(colors[x%6])     #using the different colors form color
     t.width(x//20)
     t.forward(x)
     t.left(50)
     
+# youtube link is availabe in read me file
